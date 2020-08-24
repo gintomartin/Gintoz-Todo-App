a145d1d7a3446b82ea90907e46247c85100d73f3
gintomartin <gintomartin@gmail.com>
2020-08-24 22:46:49 +0530

    Updates

---
+++ precache-manifest.f3476e42343327988d4dd1b4c34c067c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bcd5ad1c81d142062cae8922be1320",
+    "url": "/Gintoz-Todo-App/index.html"
+  },
+  {
+    "revision": "2dde2a2feebc4e0ad74f",
+    "url": "/Gintoz-Todo-App/static/css/main.def35b09.chunk.css"
+  },
+  {
+    "revision": "7c04e9fcd95ab2f3599c",
+    "url": "/Gintoz-Todo-App/static/js/2.ed75f1c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gintoz-Todo-App/static/js/2.ed75f1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dde2a2feebc4e0ad74f",
+    "url": "/Gintoz-Todo-App/static/js/main.d9144c7e.chunk.js"
+  },
+  {
+    "revision": "d4d5a7e8725f7593ccc9",
+    "url": "/Gintoz-Todo-App/static/js/runtime-main.6f917150.js"
+  }
+]);
